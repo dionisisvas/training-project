d7e9fc8348e2b7bb730aa1f7958d6071422c4bf1
Eri Sara <eri.s7@hotmail.com>
2017-05-10 15:46:31 +0300

    changes to  UserRepositoryImpl

--- src/main/java/com/iri/training/repository/UserRepositoryImpl.java
+++ src/main/java/com/iri/training/repository/UserRepositoryImpl.java
@@ -1,61 +1,62 @@
 package com.iri.training.repository;
 
-import org.springframework.stereotype.Repository;
-import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import com.iri.training.model.User;
-import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.springframework.stereotype.Repository;
+
+import com.iri.training.model.User;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
-    private static JdbcTemplate jdbcTemplate;
+    public static void main( String args[] )
+    {
 
-    @Override
-    public User getUserById(Long userId) {
-        System.out.println("success!!");
-        Connection connection = null;
 
+
+	}
+
+    public  User getUserById(Long userId) throws SQLException {
+
+        Connection c = null;
+        Statement stmt = null;
         try {
             Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlite:C:\\sqlite\\test.db");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+            c = DriverManager.getConnection("jdbc:sqlite:C:\\sqlite\\test.db");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
 
-        try {
-            if (connection != null) connection.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            stmt = c.createStatement();
+            String sql="SELECT * FROM USERS WHERE usrID= ?;";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setLong(1, userId);
+            ResultSet rs = pst.executeQuery( );
+            while ( rs.next() ) {
+                int id = rs.getInt("usrID");
+                String  Name = rs.getString("name");
+                String Surname  = rs.getString("surname");
+                String  Username = rs.getString("usrname");
+                String Password = rs.getString("password");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + Name );
+                System.out.println( "surname= " + Surname );
+                System.out.println( "usrname = " + Username );
+                System.out.println( "password = " + Password );
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
         }
+        System.out.println("Operation done successfully");
+
         return null;
     }
-
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    public static String getName() {
-        return jdbcTemplate.queryForObject("select name from USERS", String.class);
-    }
-
-    public static String getSurname() {
-        return jdbcTemplate.queryForObject("select surname from USERS", String.class);
-    }
-
-    public static String getUsername() {
-        return jdbcTemplate.queryForObject("select usrname from USERS", String.class);
-    }
-
-    public static String getPassword() {
-        return jdbcTemplate.queryForObject("select password from USERS", String.class);
-
-    }
-
-
 }
--- src/main/java/com/iri/training/web/service/UserService.java
+++ src/main/java/com/iri/training/web/service/UserService.java
@@ -1,10 +1,13 @@
 package com.iri.training.web.service;
 import com.iri.training.model.User;
+import com.iri.training.repository.UserRepositoryImpl;
 
 import java.sql.SQLException;
 
+
 public interface UserService {
 
-	User getUserById(Long userId) throws SQLException;
-		
+
+	User getUserById( Long userId) throws SQLException;
+
 }
