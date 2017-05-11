4d2b2c5d3c9d429c73fafb59ff6bb899c3d59141
Eri Sara <eri.s7@hotmail.com>
2017-05-11 11:29:54 +0300

    Revert "Revert "e""
    This reverts commit d7714c928dd23be39312f7da2733e12f50242fb9.

--- src/main/java/com/iri/training/web/service/UserService.java
+++ src/main/java/com/iri/training/web/service/UserService.java
@@ -1,6 +1,6 @@
 package com.iri.training.web.service;
 import com.iri.training.model.User;
-import com.iri.training.repository.UserRepositoryImpl;
+
 
 import java.sql.SQLException;
 
@@ -9,5 +9,8 @@ public interface UserService {
 
 
 	User getUserById( Long userId) throws SQLException;
+
+
+
 
 }
