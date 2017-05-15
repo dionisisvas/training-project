e77e876469609a36df54b97bb76e1622410f68b7
chris <christos.daramouskas@iriworldwide.com>
2017-05-15 13:13:54 +0300

    User-list avatars, CSS attributes

--- src/main/webapp/resources/app/user-info/user-info.component.js
+++ src/main/webapp/resources/app/user-info/user-info.component.js
@@ -4,11 +4,11 @@ angular.
 	module('userInfo').
 	component('userInfo', {
 		templateUrl: '../resources/app/user-info/user-info.template.html',
-		controller: function UserInfoController($http) {
+		controller: ['$http', function UserInfoController($http) {
 			var self = this;
 			
 			$http.get('../resources/app/users/users.json').then(function(response) {
 				self.users = response.data;
 			});
-		}
+		}]
 	});
--- src/main/webapp/resources/app/user-list/user-list.component.js
+++ src/main/webapp/resources/app/user-list/user-list.component.js
@@ -4,11 +4,13 @@ angular.
 	module('userList').
 	component('userList', {
 		templateUrl: 'resources/app/user-list/user-list.template.html',
-		controller: function UserListController($http) {
+		controller: ['$http', function UserListController($http) {
 			var self = this;
 			
-			$http.get('resources/app/users/users.json').then(function(response) {
+			self.orderProp = 'id';
+
+			$http.get('resources/json/users/users.json').then(function(response) {
 				self.users = response.data;
 			});
-		}
+		}]
 	});
