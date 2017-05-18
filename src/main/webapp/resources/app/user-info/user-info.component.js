ecea0a38ce58a93d403fbb1a02fa258db004ac8c
chris <christos.daramouskas@iriworldwide.com>
2017-05-18 13:27:04 +0300

    user-info template

--- src/main/webapp/resources/app/user-info/user-info.component.js
+++ src/main/webapp/resources/app/user-info/user-info.component.js
@@ -4,7 +4,12 @@ angular.
 	module('myUserInfo').
 	component('myUserInfo', {
 		templateUrl: 'resources/app/user-info/user-info.template.html',
-		controller: ['$routeParams', function UserInfoController($routeParams) {
-			this.userId = $routeParams.userId;
+		controller: ['$http', '$routeParams', function UserInfoController($http, $routeParams) {
+			var self = this;			
+			self.userId = $routeParams.userId;
+			
+			$http.get('resources/json/users/user_' + self.userId + '.json').then(function(response) {
+				self.user = response.data;
+			});
 		}]
 	});
