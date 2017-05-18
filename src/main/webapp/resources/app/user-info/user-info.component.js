0be9cc4487f435d9b51420f47814e4731d89056a
chris <christos.daramouskas@iriworldwide.com>
2017-05-18 14:37:29 +0300

    user-info thumbnail click to view support

--- src/main/webapp/resources/app/user-info/user-info.component.js
+++ src/main/webapp/resources/app/user-info/user-info.component.js
@@ -6,10 +6,14 @@ angular.
 		templateUrl: 'resources/app/user-info/user-info.template.html',
 		controller: ['$http', '$routeParams', function UserInfoController($http, $routeParams) {
 			var self = this;			
-			self.userId = $routeParams.userId;
 			
-			$http.get('resources/json/users/user_' + self.userId + '.json').then(function(response) {
+			self.setImage = function setImage(imageUrl) {
+				self.mainImageUrl = imageUrl;
+			};
+			
+			$http.get('resources/json/users/user_' + $routeParams.userId + '.json').then(function(response) {
 				self.user = response.data;
+				self.setImage(self.user.images[0]);
 			});
 		}]
 	});
