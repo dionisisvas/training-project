d191c1c7baad029446e18a3e49b5518cbc17f1ae
Eri Sara <eri.s7@hotmail.com>
2017-05-30 13:49:26 +0300

    Added mapRow method

--- src/main/java/com/iri/training/repository/UserCommentRepositoryImpl.java
+++ src/main/java/com/iri/training/repository/UserCommentRepositoryImpl.java
@@ -1,14 +1,17 @@
 package com.iri.training.repository;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.iri.training.model.UserComment;
+import com.iri.training.model.builder.UserCommentBuilder;
 
 @Repository
 public class UserCommentRepositoryImpl implements UserCommentRepository{
@@ -32,13 +35,11 @@ public class UserCommentRepositoryImpl implements UserCommentRepository{
 
 			String sql="SELECT * FROM USER_COMMENT WHERE userID= ?;";
 		    jdbcTemplate=new JdbcTemplate(dataSource);
-			userComment=jdbcTemplate.queryForObject(sql,new Object[]{userId},UserComment.class);
+			userComment=jdbcTemplate.queryForObject(sql,new Object[]{userId},new UserCommentMapper());
 
 		return userComment;
 
 	}
-
-
 
 	@Override
 	public UserComment createUserComment(final UserComment userComment) throws SQLException {
@@ -52,5 +53,14 @@ public class UserCommentRepositoryImpl implements UserCommentRepository{
 			logger.debug("EXITING createUserComment" + userComment.toString());
 
 		return null;
+	}
+	private static final class UserCommentMapper implements RowMapper<UserComment> {
+		UserComment userComment;
+		@Override public UserComment mapRow(final ResultSet resultSet, final int i) throws SQLException {
+
+			userComment= new UserCommentBuilder().withDescription(resultSet.getString("description")).withDate(resultSet.getString("commentDate"))
+					.withCommID(resultSet.getInt("commentID")).withUserID(resultSet.getInt("userID")).build();
+			return userComment;
+		}
 	}
 }
