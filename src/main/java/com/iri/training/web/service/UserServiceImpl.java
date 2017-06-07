231232f3c17208a17efe3bcedc030aac95b97d3b
Eri Sara <eri.s7@hotmail.com>
2017-06-07 11:17:58 +0300

    Changes to UserServiceImpl

--- src/main/java/com/iri/training/web/service/UserServiceImpl.java
+++ src/main/java/com/iri/training/web/service/UserServiceImpl.java
@@ -7,19 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.iri.training.model.User;
-import com.iri.training.repository.UserCommentRepositoryImpl;
 import com.iri.training.repository.UserRepository;
 
 @Service
 public  class UserServiceImpl implements UserService {
-	Logger logger = Logger.getLogger(UserCommentRepositoryImpl.class);
+	Logger logger = Logger.getLogger(UserServiceImpl.class);
 
 	@Autowired
 	UserRepository userRepository;
 	
 	@Override
 	public User getUserById(Long userId) throws SQLException {
-		logger.debug("ENTERED getUserCommentById: " + userId);
+		logger.debug("ENTERED getUserById: " + userId);
 
 		 User user = userRepository.getUserById(userId);
 		logger.debug("EXITING getUserById " + user);
