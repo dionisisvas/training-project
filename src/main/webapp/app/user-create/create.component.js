d1a379eb7e537725a40e910b2c951ff355970c36
Eri Sara <eri.s7@hotmail.com>
2017-06-14 18:08:38 +0300

    Password validation

--- src/main/webapp/app/user-create/create.component.js
+++ src/main/webapp/app/user-create/create.component.js
@@ -5,6 +5,7 @@ angular.
 		templateUrl: 'app/user-create/index.html',
 		controller: ['$scope', '$http',
 		function CreateController($scope, $http) {
+
         $scope.send_form = function () {
 
 
@@ -14,10 +15,35 @@ angular.
 		userId : $scope.userId,
 		name : $scope.name,
 		surname : $scope.surname,
+		age : $scope.age,
+		dob : $scope.dob,
+		phoneNo : $scope.phoneNo,
+		address : $scope.address,
+		password : $scope.password
+
 
 });
 $http.put('http://localhost:8080/spring/api/user/create',user);
 }
+ $scope.validatePassword=function () {
+ jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
+});
+            var validator = $("#myform").validate({
+                    rules: {
+            password : {
+                            minlength : 5
+                        },
+            password_again: { minlength : 5,
+                            equalTo : "#password"
+            }
+          }
+                });
+                if (validator.form()) {
+                    alert('Sucess');
+                }
+            }
 }]
 });
 
