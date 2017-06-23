bcbb20528508a3dbab31f16ef8c014cf593d8f10
Eri Sara <eri.s7@hotmail.com>
2017-06-23 16:52:46 +0300

    Remove Statistics support

--- src/main/java/com/iri/training/web/service/StatisticsService.java
+++
@@ -1,9 +0,0 @@
-package com.iri.training.web.service;
-
-import java.sql.SQLException;
-
-import com.iri.training.model.Statistics;
-
-public interface StatisticsService {
-	Statistics getUsersAge() throws SQLException;
-}
