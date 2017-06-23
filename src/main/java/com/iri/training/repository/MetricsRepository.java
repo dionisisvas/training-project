7ca08cfbc7fede8be9244e4fce5dafae4c6894ea
Eri Sara <eri.s7@hotmail.com>
2017-06-23 14:33:54 +0300

    Added getAllMetrics method

--- src/main/java/com/iri/training/repository/MetricsRepository.java
+++ src/main/java/com/iri/training/repository/MetricsRepository.java
@@ -1,6 +1,7 @@
 package com.iri.training.repository;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.iri.training.model.Metrics;
 
@@ -8,5 +9,5 @@ public interface MetricsRepository {
 
 	 Metrics getMetricsByUserId(Long userId) throws SQLException;
 
-
+	List <Metrics> getMetricsList() throws SQLException;
 }
--- src/main/java/com/iri/training/web/service/MetricsService.java
+++ src/main/java/com/iri/training/web/service/MetricsService.java
@@ -1,9 +1,11 @@
 package com.iri.training.web.service;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.iri.training.model.Metrics;
 
 public interface  MetricsService {
 	Metrics getMetricsByUserId(Long userId) throws SQLException;
+	List<Metrics> getMetricsList() throws SQLException;
 }
