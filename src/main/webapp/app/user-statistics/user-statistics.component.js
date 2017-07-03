2c998d8a711bad6d809c1f6ee6d8766fa56e8a31
Eri Sara <eri.s7@hotmail.com>
2017-07-03 18:16:45 +0300

    Added Charts

--- src/main/webapp/app/user-statistics/user-statistics.component.js
+++ src/main/webapp/app/user-statistics/user-statistics.component.js
@@ -1,98 +1,148 @@
 'use strict';
 
 angular.
-	module('myUserStatistics').
-	component('myUserStatistics', {
-		templateUrl: 'app/user-statistics/user-statistics.template.html',
-		controller: ['User','Metrics',
-			function UserStatisticList( User,Metrics) {
-				var self =$(this);
+   module('myUserStatistics').
+   component('myUserStatistics', {
+      templateUrl: 'app/user-statistics/user-statistics.template.html',
+      controller: ['User','Metrics','$scope',
+         function UserStatisticList( User,Metrics,$scope) {
+         $scope.chartOptions = [{
+                 id: 1,
+                 name: "Age"
+             }, {
+                 id: 2,
+                 name: "Height"
+             },{
+                id: 3,
+                name: "Weight"
+                },{
+                id: 4,
+                 name: "Education"
+             }];
 
-			self.first=0;
-		    self.second=0;
-			self.third=0;
-			self.forth=0;
-			self.fifth=0;
-			self.sixth=0;
-			self.seventh=0;
-			self.heightShort=0;
-            self.heightMedium=0;
-            self.heightTall=0;
-            self.thin=0;
-            self.medium=0;
-            self.fat=0;
-            self.university=0;
-            self.school=0;
-            self.doctorate=0;
-            self.master=0;
-            self.noDiploma=0;
-            self.professional=0;
-				self.users = User.UserList.query(function() {
-               	angular.forEach(self.users, function(user, key) {
-               	if (user.age<=24 && user.age>=18){self.first++}
-               	else if(user.age<=34 && user.age>=25){self.second++;}
-               	else if(user.age<=44 && user.age>=35){self.third++;}
-               	else if(user.age<=54 && user.age>=45){self.forth++;}
-               	else if(user.age<=64 && user.age>=55){self.fifth++;}
-               	else if(user.age<=74 && user.age>=65){self.sixth++;}
-               	else if(user.age>=75) {self.seventh++;}
+         var self =$(this);
 
-               	  google.charts.load('visualization', '1', {packages:["corechart"]});
+         self.first=0;
+         self.second=0;
+         self.third=0;
+         self.forth=0;
+         self.fifth=0;
+         self.sixth=0;
+         self.seventh=0;
+         self.heightShort=0;
+         self.heightMedium=0;
+         self.heightTall=0;
+         self.thin=0;
+         self.medium=0;
+         self.fat=0;
+         self.university=0;
+         self.school=0;
+         self.doctorate=0;
+         self.master=0;
+         self.noDiploma=0;
+         self.professional=0;
+         self.users = User.UserList.query(function() {
+                   angular.forEach(self.users, function(user, key) {
+                   if (user.age<=24 && user.age>=18){self.first++}
+                   else if(user.age<=34 && user.age>=25){self.second++;}
+                   else if(user.age<=44 && user.age>=35){self.third++;}
+                   else if(user.age<=54 && user.age>=45){self.forth++;}
+                   else if(user.age<=64 && user.age>=55){self.fifth++;}
+                   else if(user.age<=74 && user.age>=65){self.sixth++;}
+                   else if(user.age>=75) {self.seventh++;}
+
+        google.charts.load('visualization', '1', {packages:["corechart"]});
+
+
                    google.charts.setOnLoadCallback(drawChart);
-                      function drawChart() {
 
-                        var data = google.visualization.arrayToDataTable([
+                      function drawChart(x,y) {
+                          var dataAge = google.visualization.arrayToDataTable([
                           ['Task', 'User Age'],
                           ['18-24 Years Old', self.first],
                           ['25-34 years old', self.second],
                           ['35-44 years old', self.third],
                           ['45-54 years old', self.forth],
                           ['55-64 years old', self.fifth],
-                		  ['65-74 years old', self.sixth],
-                		  ['75 years or older',self.seventh]
+                          ['65-74 years old', self.sixth],
+                          ['75 years or older',self.seventh]
+                          ]);
+
+                          var dataHeight = google.visualization.arrayToDataTable([
+                          ['Task', 'User Height'],
+                          ['Short (Height<1.70)', self.heightShort],
+                          ['Medium (1.70<Height<1.80)', self.heightMedium],
+                          ['Tall (Height>1.80)' , self.heightTall]
                         ]);
 
-                        var options = {
+                    var dataWeight = google.visualization.arrayToDataTable([
+                          ['Task', 'User Weight'],
+                          ['Thin (BMI<18.5)', self.thin],
+                          ['Medium (18.5<BMI<25)', self.medium],
+                          ['Fat(BMI>25)', self.fat]
+                        ]);
+
+                    var dataEdu = google.visualization.arrayToDataTable([
+                          ['Task', 'User Education'],
+                          ['University Education', self.university],
+                          ['High School Diploma', self.school],
+                          ['Doctorate Degree', self.doctorate],
+                          ['Master Degree', self.master],
+                          ['No High School Diploma', self.noDiploma],
+                          ['Professional Degree', self.professional]
+                          ]);
+
+                        var options1 = {
                           title: 'User Age',
                           is3D: true,
                         };
-
+                        var options2 = {
+                          title: 'User Height',
+                          is3D: true,
+                        };
+                        var options3 = {
+                           title: 'User Weight',
+                           is3D: true,
+                        };
+                        var options4 = {
+                           title: 'User Education',
+                           is3D: true,
+                        };
                         var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-                        chart.draw(data, options);
+                                chart.draw(dataAge, options1);
+
+                               $scope.updateChart = function () {
+                                if ($scope.selectedChart.chart === null || $scope.selectedChart.chart.id === 1) {
+                                    x = dataAge;
+                                    y=options1;
+                                }
+
+                                if ($scope.selectedChart.chart !== undefined && $scope.selectedChart.chart.id === 2) {
+                                    x = dataHeight;
+                                    y=options2;
+                                }
+                                if ($scope.selectedChart.chart !== undefined && $scope.selectedChart.chart.id === 3) {
+                                    x = dataWeight;
+                                    y=options3;
+                                 }
+                                if ($scope.selectedChart.chart !== undefined && $scope.selectedChart.chart.id === 4) {
+                                    x = dataEdu;
+                                    y=options4;
+                                }
+                                chart.draw(x, y);
+                                self.chart1.draw(x, y);
+                               };
+
+
 
                  google.charts.load('current',{'packages':['bar']});
                  google.charts.setOnLoadCallback(drawStuff);
 }
+
             function drawStuff() {
-             var data = new google.visualization.arrayToDataTable([
-               ['User Age', 'Users'],
-               ["18-24 Years Old", self.first],
-               ["25-34 years old", self.second],
-               ["35-44 years old", self.third],
-               ["45-54 years old", self.forth],
-               ["55-64 years old", self.fifth],
-               ["65-74 years old", self.sixth],
-               ["75 years or older",self.seventh]
-             ]);
-
-             var options = {
-               title: 'User Age',
-               width: 900,
-               legend: { position: 'none' },
-               chart: { title: 'User Age'},
-               bars: 'vertical',
-               axes: {
-                 x: {
-                   0: { side: 'bottom', label: 'Percentage'} // Top x-axis.
-                 }
-               },
-               bar: { groupWidth: "90%" }
-             };
-
-             var chart = new google.charts.Bar(document.getElementById('top_x_div'));
-             chart.draw(data, options);
+            self.chart1 = new google.charts.Bar(document.getElementById('top_x_div'));
            };
-  })
+})
   });
 
   self.metrics = Metrics.MetricsList.query(function() {
@@ -111,37 +161,9 @@ else if((metrics.education).valueOf()==("Doctorate Degree").valueOf()){self.doct
 else if((metrics.education).valueOf()==("Master's Degree").valueOf()){self.master++;}
 else if((metrics.education).valueOf()==("No High School Diploma").valueOf()){self.noDiploma++;}
 else if((metrics.education).valueOf()==("Professional Degree").valueOf()){self.professional++;}
-                   google.charts.load('visualization', '1', {packages:["corechart"]});
-                   google.charts.setOnLoadCallback(drawChart1);
-                      function drawChart1() {
 
-                        var data = google.visualization.arrayToDataTable([
-                          ['Task', 'Height'],
-                          ['High School Diploma', self.school],
-                          ['Medium',self.doctorate],
-                          ['Fat', self.professional]
-                        ]);
-
-                        var options = {
-                          title: 'User Weight',
-                          is3D: true,
-                        };
-
-                        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d1'));
-                        chart.draw(data, options);
-
-  }
   });
   });
 
-
-  function myFunction() {
-      var x = document.getElementById('piechart_3d');
-      if (x.style.display === 'none') {
-          x.style.display = 'block';
-      } else {
-          x.style.display = 'none';
-      }
-  }
   }]
-  });
+  })
