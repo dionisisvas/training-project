20317bb546aba29b423c01bf49325721379a16a2
chris <christos.daramouskas@iriworldwide.com>
2017-08-18 18:31:45 +0300

    Counter variables groupped into arrays

--- src/main/webapp/app/user-statistics/user-statistics.component.js
+++ src/main/webapp/app/user-statistics/user-statistics.component.js
@@ -28,121 +28,129 @@ angular.
                     }];
                 self.selectedChart = null;
 
-                self.agedUnder25 = 0;
-                self.aged25To34 = 0;
-                self.aged35To44 = 0;
-                self.aged45To54 = 0;
-                self.aged55To64 = 0;
-                self.aged65To74 = 0;
-                self.agedOver74 = 0;
+                self.ageGroups = [
+                        0, // Aged under 25 years old
+                        0, // Aged between 25 to 34 years old
+                        0, // Aged between 35 to 44 years old
+                        0, // Aged between 45 to 54 years old
+                        0, // Aged between 55 to 64 years old
+                        0, // Aged between 65 to 74 years old
+                        0  // Aged over 74 years old
+                    ];
 
-                self.heightShort = 0;
-                self.heightMedium = 0;
-                self.heightTall = 0
+                self.heightGroups = [
+                        0, // Short - Height at most 1.70m
+                        0, // Average - Height at most 1.80m
+                        0  // Tall - Height over 1.80m
+                    ];
 
-                self.thin = 0;
-                self.medium = 0;
-                self.fat = 0;
+                self.weightGroups = [
+                        0, // Underweight - 18.5 BMI at most
+                        0, // Normal weight - 25 BMI at most
+                        0  // Overweight - Over 25 BMI
+                    ];
 
-                self.university = 0;
-                self.school = 0;
-                self.doctorate = 0;
-                self.master = 0;
-                self.noDiploma = 0;
-                self.professional = 0;
+                self.educationGroups = [
+                        0, // Illiterate
+                        0, // Finished high school
+                        0, // Has a professional degree
+                        0, // Has a bachelor's degree
+                        0, // Has a master's degree
+                        0  // Has a doctorate's degree
+                    ];
 
                 self.users = User.UserList.query(function() {
                     angular.forEach(self.users, function(user) {
 
                         if (user.age < 25) {
-                            self.agedUnder25++;
-                        } else if(user.age < 35) {
-                            self.aged25To34++;
-                        } else if(user.age < 45) {
-                            self.aged35To44++;
-                        } else if(user.age < 55) {
-                            self.aged45To54++;
-                        } else if(user.age < 65) {
-                            self.aged55To64++;
-                        } else if(user.age < 75) {
-                            self.aged65To74++;
+                            self.ageGroups[0]++;
+                        } else if (user.age < 35) {
+                            self.ageGroups[1]++;
+                        } else if (user.age < 45) {
+                            self.ageGroups[2]++;
+                        } else if (user.age < 55) {
+                            self.ageGroups[3]++;
+                        } else if (user.age < 65) {
+                            self.ageGroups[4]++;
+                        } else if (user.age < 75) {
+                            self.ageGroups[5]++;
                         } else {
-                            self.agedOver74++;
+                            self.ageGroups[6]++;
                         }
 
                         self.metrics = Metrics.MetricsList.query(function() {
 
-                            angular.forEach(self.metrics, function(metric, key) {
+                            angular.forEach(self.metrics, function(metric) {
                                 if (metric.height <= 1.70) {
-                                    self.heightShort++;
-                                } else if(metric.height <= 1.80) {
-                                    self.heightMedium++;
+                                    self.heightGroups[0]++;
+                                } else if (metric.height <= 1.80) {
+                                    self.heightGroups[1]++;
                                 } else {
-                                    self.heightTall++;
+                                    self.heightGroups[2]++;
                                 }
 
                                 metric.bmi = metric.weight / (metric.height * metric.height);
 
                                 if (metric.bmi <= 18.5) {
-                                    self.thin++;
+                                    self.weightGroups[0]++;
                                 } else if (metric.bmi <= 25) {
-                                    self.medium++;
+                                    self.weightGroups[1]++;
                                 } else {
-                                    self.fat++;
+                                    self.weightGroups[2]++;
                                 }
 
-                                if (metric.education === "University Education") {
-                                    self.university++;
-                                } else if(metric.education === "High School Diploma") {
-                                    self.school++;
-                                } else if(metric.education === "Doctorate Degree") {
-                                    self.doctorate++;
-                                } else if(metric.education === "Master's Degree") {
-                                    self.master++;
-                                } else if(metric.education === "No High School Diploma") {
-                                    self.noDiploma++;
-                                } else if(metric.education === "Professional Degree") {
-                                    self.professional++;
+                                if (metric.education === "No High School Diploma") {
+                                    self.educationGroups[0]++;
+                                } else if (metric.education === "High School Diploma") {
+                                    self.educationGroups[1]++;
+                                } else if (metric.education === "Professional Degree") {
+                                    self.educationGroups[2]++;
+                                } else if (metric.education === "University Education") {
+                                    self.educationGroups[3]++;
+                                } else if (metric.education === "Master's Degree") {
+                                    self.educationGroups[4]++;
+                                } else if (metric.education === "Doctorate Degree") {
+                                    self.educationGroups[5]++;
                                 }
                             });
 
                             google.charts.load('visualization', '1', {packages:["corechart"]});
                             google.charts.setOnLoadCallback(drawChart);
 
-                            function drawChart(x,y) {
+                            function drawChart(x, y) {
                                 self.dataAge = google.visualization.arrayToDataTable([
                                         ['Task',                'User Age'],
-                                        ['Under 25 years old',  self.agedUnder25],
-                                        ['25-34 years old',     self.aged25To34],
-                                        ['35-44 years old',     self.aged35To44],
-                                        ['45-54 years old',     self.aged45To54],
-                                        ['55-64 years old',     self.aged55To64],
-                                        ['65-74 years old',     self.aged65To74],
-                                        ['Over 74 years old',   self.agedOver74]
+                                        ['Under 25 years old',  self.ageGroups[0]],
+                                        ['25-34 years old',     self.ageGroups[1]],
+                                        ['35-44 years old',     self.ageGroups[2]],
+                                        ['45-54 years old',     self.ageGroups[3]],
+                                        ['55-64 years old',     self.ageGroups[4]],
+                                        ['65-74 years old',     self.ageGroups[5]],
+                                        ['Over 74 years old',   self.ageGroups[6]]
                                     ]);
 
                                 self.dataHeight = google.visualization.arrayToDataTable([
                                         ['Task',                    'User Height'],
-                                        ['Short  (Height <= 1.70)',  self.heightShort],
-                                        ['Medium (Height <= 1.80)',  self.heightMedium],
-                                        ['Tall   (Height > 1.80)' ,  self.heightTall]
+                                        ['Short  (Height <= 1.70)',  self.heightGroups[0]],
+                                        ['Medium (Height <= 1.80)',  self.heightGroups[1]],
+                                        ['Tall   (Height > 1.80)' ,  self.heightGroups[2]]
                                     ]);
 
                                 self.dataWeight = google.visualization.arrayToDataTable([
                                         ['Task',                  'User Weight'],
-                                        ['Thin   (BMI <= 18.5)',  self.thin],
-                                        ['Medium (BMI <= 25)',    self.medium],
-                                        ['Fat    (BMI > 25)',     self.fat]
+                                        ['Thin   (BMI <= 18.5)',  self.weightGroups[0]],
+                                        ['Medium (BMI <= 25)',    self.weightGroups[1]],
+                                        ['Fat    (BMI > 25)',     self.weightGroups[2]]
                                     ]);
 
                                 self.dataEdu = google.visualization.arrayToDataTable([
                                         ['Task',                    'User Education'],
-                                        ['University Education',    self.university],
-                                        ['High School Diploma',     self.school],
-                                        ['Doctorate Degree',        self.doctorate],
-                                        ['Master Degree',           self.master],
-                                        ['No High School Diploma',  self.noDiploma],
-                                        ['Professional Degree',     self.professional]
+                                        ['University Education',    self.educationGroups[3]],
+                                        ['High School Diploma',     self.educationGroups[1]],
+                                        ['Doctorate Degree',        self.educationGroups[5]],
+                                        ['Master Degree',           self.educationGroups[4]],
+                                        ['No High School Diploma',  self.educationGroups[0]],
+                                        ['Professional Degree',     self.educationGroups[2]]
                                     ]);
 
                                 self.chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
@@ -155,15 +163,15 @@ angular.
                                     }
 
                                     if ($scope.selectedChart.id === 2) {
-                                        x = dataHeight;
+                                        x = self.dataHeight;
                                         y = self.chartOptions[1];
                                     }
                                     if ($scope.selectedChart.id === 3) {
-                                        x = dataWeight;
+                                        x = self.dataWeight;
                                         y = self.chartOptions[2];
                                      }
                                     if ($scope.selectedChart.id === 4) {
-                                        x = dataEdu;
+                                        x = self.dataEdu;
                                         y = self.chartOptions[3];
                                     }
 
