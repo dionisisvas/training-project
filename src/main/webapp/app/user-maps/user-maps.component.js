6ca2fc5d746df2632169924e1a4a6482bb099af6
chris <christos.daramouskas@iriworldwide.com>
2017-08-22 10:47:04 +0300

    Map component clean up

--- src/main/webapp/app/user-maps/user-maps.component.js
+++ src/main/webapp/app/user-maps/user-maps.component.js
@@ -1,74 +1,72 @@
 'use strict';
 
 angular.
-   module('myUserMaps').
-   component('myUserMaps', {
-      templateUrl: 'app/user-maps/user-maps.template.html',
-      controller: ['Metrics',
-  function UserMaps(Metrics) {
-var a = {};
-var b=[];
-self.metrics = Metrics.MetricsList.query(function() {
-angular.forEach(self.metrics, function(metrics,key) {
-if (_.has(a,metrics.nationality)){ a[metrics.nationality]++;}
-else{a[metrics.nationality] = 1}
-b.push(metrics.place_of_birth);
-console.log(b);
-google.charts.load('current', {
-        'packages':['geochart'],
-        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-      });
-      google.charts.setOnLoadCallback(drawRegionsMap);
-     function drawRegionsMap() {
-     var d1=_.keys(a);
-     var d2=_.values(a);
-        var data =new google.visualization.DataTable();
-          data.addColumn('string', 'd1');
-            data.addColumn('number', 'Number Of Users');
-            for(var i = 0; i < d1.length; i++)
-              data.addRow([d1[i], d2[i]]);
+    module('myUserMaps').
+    component('myUserMaps', {
+        templateUrl: 'app/user-maps/user-maps.template.html',
+        controller: ['Metrics',
+            function UserMapsController(Metrics) {
+                var a = {};
+                var b = [];
+                self.metrics = Metrics.MetricsList.query(function() {
 
-        var options = {
-        };
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-        chart.draw(data, options);
-      }
+                    angular.forEach(self.metrics, function(metrics, key) {
 
- google.charts.load('current', {
-        'packages': ['geochart'],
-        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-      });
-   google.charts.setOnLoadCallback(drawMarkersMap);
-         function drawMarkersMap() {
-         var data = new google.visualization.DataTable();
-         data.addColumn('string', 'Address');
-        for(var i = 0; i < b.length; i++)
-            data.addRow([b[i]]);
+                        if (_.has(a, metrics.nationality)) {
+                            a[metrics.nationality]++;
+                        } else {
+                            a[metrics.nationality] = 1;
+                        }
+                        b.push(metrics.place_of_birth);
 
-         var options = {
-          width:700,
-           height:600,
-           displayMode: 'markers',
-           colorAxis: {colors: ['red', 'blue']},
-   		   showInfoWindow: true
-         };
+                        google.charts.load('current', {
+                                'packages':['geochart'],
+                                'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+                            });
+                        google.charts.setOnLoadCallback(drawRegionsMap);
 
-         var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
-         chart.draw(data, options);
-       };
-        });
-        });
+                        function drawRegionsMap() {
 
+                            var d1 = _.keys(a);
+                            var d2 = _.values(a);
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'd1');
+                            data.addColumn('number', 'Number Of Users');
+                            for(var i = 0; i < d1.length; i++) {
+                                data.addRow([d1[i], d2[i]]);
+                            }
 
- $(document).ready(function() {
-                 $('input:radio[name=Status]').change(function() {
-                     if($('#1').css('display')!='none'){
-                         $('#2').html($().html()).show().siblings('div').hide();
-                         }else if($('#2').css('display')!='none'){
-                             $('#1').show().siblings('div').hide();
-                         }
-  });
-});
+                            var options = {};
+                            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+                            chart.draw(data, options);
+                        };
 
-}]
-})
+                        google.charts.load('current', {
+                                'packages': ['geochart'],
+                                'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+                            });
+                        google.charts.setOnLoadCallback(drawMarkersMap);
+
+                        function drawMarkersMap() {
+
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'Address');
+                            for (var i = 0; i < b.length; i++) {
+                                data.addRow([b[i]]);
+                            }
+
+                            var options = {
+                                    width: 700,
+                                    height: 600,
+                                    displayMode: 'markers',
+                                    colorAxis: {colors: ['red', 'blue']},
+                           		      showInfoWindow: true
+                                };
+
+                            var chart = new google.visualization.GeoChart(document.getElementById('markers_div'));
+                            chart.draw(data, options);
+                        };
+                    });
+                });
+        }]
+    })
