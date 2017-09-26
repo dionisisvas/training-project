64ccd1e01003beb892c347725764a8e09bc2d3a5
chris <christos.daramouskas@iriworldwide.com>
2017-09-26 18:33:31 +0300

    Refactored the ImageService interface and implementation

--- src/main/java/com/iri/training/web/service/ImageServiceImpl.java
+++ src/main/java/com/iri/training/web/service/ImageServiceImpl.java
@@ -3,6 +3,7 @@ package com.iri.training.web.service;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +13,45 @@ import com.iri.training.repository.ImageRepository;
 @Service
 public  class ImageServiceImpl implements ImageService {
 
+	private static final Logger logger = Logger.getLogger(ImageRepository.class);
+
 	@Autowired
 	ImageRepository imgRepository;
 
 	@Override
-	public Image getImageById(Long imgId) throws SQLException {
-		return imgRepository.getImageById(imgId);
+	public final Image getImageById(final long imgId) throws SQLException {
+
+		logger.debug("ENTERED getImageById for imgId: " + imgId);
+
+		final Image userImg = imgRepository.getImageById(imgId);
+
+		logger.debug("EXITING getImageById with userImg: " + userImg);
+
+		return userImg;
 	}
 
 	@Override
-	public Image getProfileImage(Long userId) throws SQLException {
-		return 	imgRepository.getProfileImage(userId);
+	public final Image getProfileImage(final long userId) throws SQLException {
+
+		logger.debug("ENTERED getProfileImage for userId: " + userId);
+
+		final Image userProfileImg = imgRepository.getProfileImage(userId);
+
+		logger.debug("EXITING getProfileImage with userProfileImg: " + userProfileImg);
+
+		return userProfileImg;
 	}
 
 	@Override
-	public List<Image> getUserImages(Long userId) throws SQLException {
-		return 	imgRepository.getUserImages(userId);
+	public final List<Image> getUserImages(final long userId) throws SQLException {
+
+		logger.debug("ENTERED getUserImages for userId: " + userId);
+
+		final List<Image> images = imgRepository.getUserImages(userId);
+
+		logger.debug("EXITING getUserImages for userId: " + userId);
+
+		return images;
 	}
 }
 
